46259f0bad46723fc6b2151c4c6658204b665527
zeev mindali <mindalizeev@gmail.com>
2023-06-14 19:41:07 +0300

    ts

--- api-cars/index.ts
+++ api-cars/index.ts
@@ -41,45 +41,45 @@ app.get("/cars", (req: Request, res: Response, next: NextFunction) => {
 
 })
 
-// function validateTextInput(req, res, next) {
-//     const { text } = req.query
-//     if (typeof text === 'string' && text.length > 20) {
-//         next(new Error("Text length is over max characters: 20")) // go to 72
-//     } else {
-//         next()
-//     }
-// }
-// app.get("/log", validateTextInput, (req, res, next) => {
-//     try {
-//         const { text } = req.query;
-//         if (!text) throw new Error("Missing text value")
-//         fs.appendFile("log.txt", `${text}\n`, (err) => {
-//             console.log(err)
-//             if (err) throw new Error("File issue")
-//             return res.send("log written")
-//         })
-//     } catch (error) {
-//         next(error)
-//     }
-// })
+function validateTextInput(req: Request, res: Response, next: NextFunction) {
+    const { text } = req.query
+    if (typeof text === 'string' && text.length > 20) {
+        next(new Error("Text length is over max characters: 20")) // go to 72
+    } else {
+        next()
+    }
+}
+app.get("/log", validateTextInput, (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { text } = req.query;
+        if (!text) throw new Error("Missing text value")
+        fs.appendFile("log.txt", `${text}\n`, (err) => {
+            console.log(err)
+            if (err) throw new Error("File issue")
+            return res.send("log written")
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 
-// app.get("/log-file", (req, res, next) => {
-//     try {
-//         // res.download("log.txt")
-//         fs.readFile("log.txt", 'utf8', (err, data) => {
-//             console.log(data)
-//             if (err) throw new Error("File issue")
-//             return res.json({ d: data })
-//         })
-//     } catch (error) {
-//         next(error)
-//     }
-// })
+app.get("/log-file", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // res.download("log.txt")
+        fs.readFile("log.txt", 'utf8', (err, data) => {
+            console.log(data)
+            if (err) throw new Error("File issue")
+            return res.json({ d: data })
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 
-// app.use((err, req, res, next) => {
-//     console.log(err)
-//     res.status(500).send("Something went wrong")
-// })
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err)
+    res.status(500).send("Something went wrong")
+})
 
 app.listen(4000, () => {
     console.log("connected, to the port: 4000")
